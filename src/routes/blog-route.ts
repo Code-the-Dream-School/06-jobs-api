e5c0a20ee4da5231116374e8d943ebaed99fa92b
aiman <abduldanmaihaja@gmail.com>
2023-04-25 10:06:49 +0100

    made all blogs accessible to all

--- src/routes/blog-route.ts
+++ src/routes/blog-route.ts
@@ -11,9 +11,10 @@ import {
   updateBlog,
 } from "../controllers/blog-controllers";
 
+Router.route("/").get(getAllBlogs);
 Router.use(protect);
 
-Router.route("/").post(createBlog).get(getAllBlogs);
+Router.route("/").post(createBlog);
 Router.route("/my-blogs").get(getMyBlogs);
 Router.route("/comment/:id").post(addComment);
 Router.route("/:id").patch(updateBlog).get(getOneBlog).delete(deleteBlog);
