58d64ea55eb75183966aaf5a5a90118f33ebdfc5
Catherine Njenga <freshiah.catherine@gmail.com>
2023-05-22 16:56:53 +0300

    Swagger docs added

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express'),
   app = express();
 
@@ -38,8 +43,11 @@ app.use(xss());
 
 // routes
 app.get('/', (req, res) => {
-  res.send('blogs api');
+  res.send('<h1>Blogs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/blogs',authenticateUser, blogsRouter);
 
