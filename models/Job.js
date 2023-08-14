3ea1eb8503c3c8b5dc7891b937e7a57103504f26
David Villavicencio <111402717+dsvill06@users.noreply.github.com>
2023-08-14 19:07:23 -0400

    Answers week 10

--- models/Job.js
+++ models/Job.js
@@ -1,26 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const JobsSchema = mongoose.Schema({
-  company: {
-    type: String,
-    required: [true, "Please provide company name"],
-    maxlength: 50
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
   },
-  position: {
-    type: String,
-    required: [true, "Please provide position"],
-    maxlength: 100
-  },
-  status: {
-    type:String,
-    enum:['interview', 'declined','pending'],
-    default: 'pending',
-  },
-  createdBy:{
-    type:mongoose.Types.ObjectId,
-    ref:'User',
-    required:[true,"Please provide a user"]
-  }
-}, {timestamps: true});
+  { timestamps: true }
+)
 
-module.exports = mongoose.model('Job', JobsSchema)
+module.exports = mongoose.model('Job', JobSchema)
