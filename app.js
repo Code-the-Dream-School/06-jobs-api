cc2a88c7d26a1e44ef0d15b05a0aaf265abf69c6
TWOTONE222 <jukari.richardson@gmail.com>
2023-09-04 07:41:46 -0700

    Restructured file

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ require('dotenv').config({ path: './.env' });
 require("express-async-errors");
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 const express = require("express");
 const app = express();
 
 // error handler
-const notFoundMiddleware = require("../middleware/not-found");
-const errorHandlerMiddleware = require("../middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
@@ -30,7 +29,6 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 app.use(helmet());
 app.use(cors());
-app.use(xss());
 
 const port = process.env.PORT || 3000;
 
